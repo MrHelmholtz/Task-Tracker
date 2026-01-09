45d64e08decad87466749a85076c5e90d80d9413
Muramidara <gratuti007@gmail.com>
2026-01-09 09:35:31 +0300

    Added exception handling for creating of objects test methods instead of 'throw'ing them

--- App/Task Tracker/src/Main.java
+++ App/Task Tracker/src/Main.java
@@ -22,47 +22,81 @@ public class Main {
     }
 
 
-    public static Library buildTestLibrary() throws IOException{
-        Helper.scan = new Scanner(Path.of("C:\\Users\\Lenovo\\My projects\\Task_and_Goals_tracker\\App" +
-                "\\Task Tracker\\Tests\\buildTests\\libraryWithDesc.txt"));
+
+    public static void adjustLibraryTest() {
+        Library lib = buildTestLibrary();
+    }
+
+    public static Library buildTestLibrary() {
+        try{
+            Helper.scan = new Scanner(Path.of("C:\\Users\\Lenovo\\My projects\\Task_and_Goals_tracker\\App" +
+                    "\\Task Tracker\\Tests\\buildTests\\libraryWithDesc.txt"));
+
+        } catch (IOException e) {
+            System.out.println("IOException: "+e);
+        }
         Library library = builder.buildLibrary();
         Helper.scan = new Scanner(System.in);
         return library;
     }
 
 
-    public static Section buildTestSection() throws IOException{
+    public static Section buildTestSection(){
+        try{
         Helper.scan = new Scanner(Path.of("C:\\Users\\Lenovo\\My projects\\Task_and_Goals_tracker\\App" +
                 "\\Task Tracker\\Tests\\buildTests\\sectionWithDesc.txt"));
+        } catch (IOException e) {
+            System.out.println("IOException: "+e);
+        }
         Section section = builder.buildSection();
         Helper.scan = new Scanner(System.in);
         return section;
     }
-    public static Goal buildTestGoal() throws IOException{
+    public static Goal buildTestGoal() {
+        try{
+
         Helper.scan = new Scanner(Path.of("C:\\Users\\Lenovo\\My projects\\Task_and_Goals_tracker\\App" +
                 "\\Task Tracker\\Tests\\buildTests\\goalWithDesc.txt"));
+    } catch (IOException e) {
+        System.out.println("IOException: "+e);
+    }
         Goal goal = builder.buildGoal();
         Helper.scan = new Scanner(System.in);
         return goal;
     }
-    public static StrictGoal buildTestStrictGoal() throws IOException{
+    public static StrictGoal buildTestStrictGoal() {
+        try{
+
         Helper.scan = new Scanner(Path.of("C:\\Users\\Lenovo\\My projects\\Task_and_Goals_tracker\\App" +
                 "\\Task Tracker\\Tests\\buildTests\\strictGoalWithDesc.txt"));
+    } catch (IOException e) {
+        System.out.println("IOException: "+e);
+        }
         StrictGoal strictGoal = builder.buildStrictGoal();
         Helper.scan = new Scanner(System.in);
         return strictGoal;
     }
-    public static Task buildTestTask() throws IOException{
+    public static Task buildTestTask() {
+        try{
+
         Helper.scan = new Scanner(Path.of("C:\\Users\\Lenovo\\My projects\\Task_and_Goals_tracker\\App" +
                 "\\Task Tracker\\Tests\\buildTests\\taskWithDesc.txt"));
+    } catch (IOException e) {
+        System.out.println("IOException: "+e);
+        }
         Task task = builder.buildTask();
         Helper.scan = new Scanner(System.in);
         return task;
     }
 
-    public static StrictTask buildTestStrictTask() throws IOException{
+    public static StrictTask buildTestStrictTask(){
+        try{
+
         Helper.scan = new Scanner(Path.of("C:\\Users\\Lenovo\\My projects\\Task_and_Goals_tracker\\App" +
                 "\\Task Tracker\\Tests\\buildTests\\strictTaskWithDesc.txt"));
+    } catch (IOException e) {
+        System.out.println("IOException: "+e);
+        }
         StrictTask strictTask = builder.buildStrictTask();
         Helper.scan = new Scanner(System.in);
         return strictTask;
